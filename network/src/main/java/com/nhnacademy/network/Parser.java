54255bb61617f3dc2a0f4b242911cc337f3330ff
jung-ki-hun <khkh0130@gmail.com>
2022-04-21 19:13:07 +0900

    jkh 1no

--- network/src/main/java/com/nhnacademy/network/Parser.java
+++ network/src/main/java/com/nhnacademy/network/Parser.java
@@ -1,6 +1,22 @@
 package com.nhnacademy.network;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Parser {
-    String method;
+    public String date() {
+
+        String input = LocalDateTime.now().toString();
+        DateTimeFormatter formatter = DateTimeFormatter.RFC_1123_DATE_TIME;
+        LocalDateTime date = LocalDateTime.parse(input, formatter);
+
+        return date.toString();
+    }
+
+    public static void main(String[] args) {
+        Parser parser = new Parser();
+//        parser.date();
+        System.out.println(parser.date());
+    }
 
 }
--- network/src/main/java/com/nhnacademy/network/Server.java
+++ network/src/main/java/com/nhnacademy/network/Server.java
@@ -6,29 +6,26 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.OutputStreamWriter;
+
 import java.io.PrintStream;
-import java.io.PrintWriter;
+
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.time.LocalDateTime;
+
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Server {
     public String date() {
-
-        String input = LocalDateTime.now().toString();
-        DateTimeFormatter formatter = DateTimeFormatter.RFC_1123_DATE_TIME;
-        LocalDateTime date = LocalDateTime.parse(input, formatter);
-
-        return date.toString();
+        ZonedDateTime now = ZonedDateTime.now();
+        return DateTimeFormatter.RFC_1123_DATE_TIME.format(now);
     }
     public int size(String str){
         return str.length();
     }
-//    public String json(List <String> jsonData){
+    //    public String json(List <String> jsonData){
 //        String temp ="";
 //        for (String data:jsonData) {
 //            temp +=
@@ -38,9 +35,11 @@ public class Server {
     public String bodyMake(String path) {
         String temp ="";
         switch(path){
-            case "/ip":
+            case "/get":
                 String ip ="1.253.143.122";
-                temp ="{\"origin\": \""+ip+"\"\n }";
+                temp ="{\n" +
+                    "  \"origin\": \"103.243.200.16\"\n" +
+                    "}";
                 return temp;
         }
         return temp;
@@ -61,19 +60,21 @@ public class Server {
             List<String> result = new ArrayList<>();
             while ((line = br.readLine()) != null) {
                 result.add(line);
-//                System.out.println(result);
+                if (line.equals("")) break;
             }
             ////////////////////
 
             String[] splitLine = result.get(0).split(" ");
 
             String body =
-                splitLine[2] + " 200 OK\n" + "Date: " + server.date() + "Content-Type: application/json\n" +
+                splitLine[2] + " 200 OK\n" + "Date: " + server.date() +"\n" + "Content-Type: application/json\n" +
                     "Content-Length: " + server.size(server.bodyMake(splitLine[1])) + "\n" +
                     "Connection: keep-alive\n" +
                     "Server: gunicorn/19.9.0\n" +
                     "Access-Control-Allow-Origin: *\n" +
                     "Access-Control-Allow-Credentials: true\n\n" + server.bodyMake(splitLine[1])+"\n";
+
+            System.out.println(body);
 
             ps.print(body);
         } catch (IOException e) {
